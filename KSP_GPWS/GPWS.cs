e148ce65def1c8ff9d04d930ff80b985dd776d4c
bssthu <bssda03@gmail.com>
2015-01-17 02:33:15 +0800

    add GPWSGear module and load it

--- KSP_GPWS/GPWS.cs
+++ KSP_GPWS/GPWS.cs
@@ -1,7 +1,7 @@
 ﻿// GPWS mod for KSP
 // License: CC-BY-NC-SA
 // Author: bss, 2015
-// Last modified: 2015-01-17, 01:02:50
+// Last modified: 2015-01-17, 02:20:08
 
 using System;
 using System.Collections.Generic;
@@ -12,10 +12,32 @@ using UnityEngine;
 namespace KSP_GPWS
 {
     [KSPAddon(KSPAddon.Startup.Flight, false)]
-    public class GPWS : MonoBehaviour
+    public class GPWS : UnityEngine.MonoBehaviour
     {
+        private List<GPWSGear> gearList = new List<GPWSGear>();     // parts with module "GPWSGear"
+
         public void Awake()
         {
+        }
+
+        public void Start()
+        {
+            if (FlightGlobals.ActiveVessel == null)
+            {
+                return;
+            }
+
+            Vessel vessel = FlightGlobals.ActiveVessel;
+            gearList.Clear();
+            for (int i = 0; i < vessel.parts.Count; i++)    // it is said that foreach costs more memory due to Unity Mono issues
+            {
+                Part p = vessel.parts[i];
+                if (p.Modules.Contains("GPWSGear"))
+                {
+                    gearList.Add(p.Modules["GPWSGear"] as GPWSGear);
+                    Log(String.Format("find {0}", p.name));
+                }
+            }
         }
 
         public void Update()
---
+++ KSP_GPWS/GPWSGear.cs
@@ -0,0 +1,16 @@
+﻿// GPWS mod for KSP
+// License: CC-BY-NC-SA
+// Author: bss, 2015
+// Last modified: 2015-01-17, 01:57:11
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KSP_GPWS
+{
+    public class GPWSGear : PartModule
+    {
+    }
+}
