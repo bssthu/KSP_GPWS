c1c7332459595431b2882b81c4f3c045e71dbc67
bssthu <bssda03@gmail.com>
2015-01-17 01:05:24 +0800

    create project

---
+++ KSP_GPWS/GPWS.cs
@@ -0,0 +1,30 @@
+﻿// GPWS mod for KSP
+// License: CC-BY-NC-SA
+// Author: bss, 2015
+// Last modified: 2015-01-17, 01:02:50
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KSP_GPWS
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class GPWS : MonoBehaviour
+    {
+        public void Awake()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        void Log(String msg)
+        {
+            UnityEngine.Debug.Log("[GPWS] " + msg);
+        }
+    }
+}
