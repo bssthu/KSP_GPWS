b40747e8144cc2b2853ca9d9f585d0fc3ddf9562
bssthu <bssthu@users.noreply.github.com>
2016-10-16 11:03:05 +0800

    upgrade ToolbarWrapper

--- KSP_GPWS/Interfaces/ToolbarWrapper.cs
+++ KSP_GPWS/Interfaces/ToolbarWrapper.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (c) 2013-2014, Maik Schreiber
+Copyright (c) 2013-2016, Maik Schreiber
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
@@ -25,9 +25,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using UnityEngine;
 
 
@@ -403,7 +401,7 @@ namespace KSP_GPWS.Interfaces {
 	/// <example>
 	/// <code>
 	/// IButton button = ...
-	/// button.Visibility = new GameScenesVisibility(GameScenes.EDITOR, GameScenes.SPH);
+	/// button.Visibility = new GameScenesVisibility(GameScenes.EDITOR, GameScenes.FLIGHT);
 	/// </code>
 	/// </example>
 	/// <seealso cref="IButton.Visibility"/>
@@ -727,16 +725,13 @@ namespace KSP_GPWS.Interfaces {
 		}
 
 		internal static Type getType(string name) {
-			foreach (AssemblyLoader.LoadedAssembly assembly in AssemblyLoader.loadedAssemblies) {
-				try {
-					var type = assembly.assembly.GetExportedTypes().SingleOrDefault(t => t.FullName == name);
-					if (type != null)
-						return type;
+			Type type = null;
+			AssemblyLoader.loadedAssemblies.TypeOperation(t => {
+				if (t.FullName == name) {
+					type = t;
 				}
-				catch (InvalidOperationException) {
-				}
-			}
-			return null;
+			});
+			return type;
 		}
 
 		internal static PropertyInfo getProperty(Type type, string name) {
